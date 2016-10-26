ff728dc77d784aba392e5c55ad50a71c9777bc73
ningkaidu <1689687404@qq.com>
2016-10-26 09:25:59 +0800

    LoginUI

---
+++ src/zju/ningkai/web/UI/LoginUI.java
@@ -0,0 +1,44 @@
+package zju.ningkai.web.UI;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginUI
+ * @author ningkai
+ * @since 2016-10-26
+ */
+@WebServlet("/Login")
+public class LoginUI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginUI() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		request.getRequestDispatcher("/html/login.html").forward(request,response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
